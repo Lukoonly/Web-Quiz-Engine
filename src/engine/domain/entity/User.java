b428c04aa6bc9a0228f423f5ebb263fe6f2c4b4d
admin <admin@MacBook-Pro-admin-2.local>
2022-09-03 19:18:00 +0300

    add project

---
+++ src/engine/domain/entity/User.java
@@ -0,0 +1,23 @@
+package engine.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String email;
+    private String password;
+    @OneToMany(mappedBy = "user")
+    private List<Quiz> quizList = new ArrayList<>();
+}
---
+++ src/engine/domain/repository/UserRep.java
@@ -0,0 +1,15 @@
+package engine.domain.repository;
+
+import engine.domain.entity.Quiz;
+import engine.domain.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRep extends CrudRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
+
+}
